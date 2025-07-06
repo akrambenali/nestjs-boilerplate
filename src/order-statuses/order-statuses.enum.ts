9c394e94dab27d7884c3778e4283a28b04b4793f
akram benali <akram1304@gmail.com>
2025-07-06 19:33:00 +0200

    chore:  remove  order and  order  item

--- src/order-statuses/order-statuses.enum.ts
+++ src/order-statuses/order-statuses.enum.ts
@@ -1,13 +1,5 @@
 export enum OrderStatusEnum {
-  'brouillon' = 1,
-  'en_attente_de_paiement' = 2,
-  'payee' = 3,
-  'en_preparation' = 4,
-  'prete_a_retirer' = 5,
-  'en_livraison' = 6,
-  'livree' = 7,
-  'annulee' = 8,
-  'echouee' = 9,
-  'remboursee' = 10,
-  'partiellement_livree' = 11,
+  'en_preparation' = 1,
+  'en_livraison' = 2,
+  'livree' = 3,
 }
